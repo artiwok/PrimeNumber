6b419ff2c58cb63ed96abe7b357eae8315d70cf6
artiwok <artemolga@ukr.net>
2020-11-15 23:37:31 +0200

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static bool PrimeNumber(int num)
+        {
+            if (num < 2)
+            {
+                Console.WriteLine($"Number {num} is NOT prime\n");
+
+                return false;
+            }
+
+            //for (int i = 2, max = Convert.ToInt32(Math.Sqrt(num)); i <= max; i++)
+            for (int i = 2; i * i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine($"Number {num} is NOT prime\n");
+
+                    return false;
+                }
+            }
+
+            Console.WriteLine($"Number {num} is prime\n");
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            PrimeNumber(23);   
+        }
+    }
+}
